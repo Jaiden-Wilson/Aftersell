9daebc747ab20a33997ef4c37b47f0d6521e4672
Bart van den Aardweg <bartvandenaardweg@gmail.com>
2021-07-24 23:28:35 +0200

    feat: structured data

--- src/components/common/Blocks/QuestionList.tsx
+++ src/components/common/Blocks/QuestionList.tsx
@@ -1,4 +1,7 @@
 import { Box, SystemProps } from '@storyofams/react-ui';
+import { getPlainText } from '@storyofams/storyblok-toolkit';
+import { FAQPageJsonLd } from 'next-seo';
+
 import { Accordion } from '../Accordion';
 import { RichText } from './RichText';
 
@@ -14,6 +17,13 @@ export const QuestionList = ({ content, ...props }: QuestionListProps) => {
           <RichText fontSize={[1.75, 2]} text={question?.answer} />
         </Accordion>
       ))}
+
+      <FAQPageJsonLd
+        mainEntity={content?.map((question) => ({
+          questionName: question?.question,
+          acceptedAnswerText: getPlainText(question?.answer),
+        }))}
+      />
     </Box>
   );
 };
