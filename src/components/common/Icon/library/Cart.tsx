6fec3d7ccb61207d2c14a7cbb3a01b758309591b
Bart van den Aardweg <bartvandenaardweg@gmail.com>
2021-07-27 17:26:31 +0200

    chore(icons): update

--- src/components/common/Icon/library/Cart.tsx
+++ src/components/common/Icon/library/Cart.tsx
@@ -12,6 +12,9 @@ function Cart() {
       <path
         d="M8 22a1 1 0 100-2 1 1 0 000 2zM17 22a1 1 0 100-2 1 1 0 000 2zM1 5h3.273l2.192 10.712c.075.368.28.699.58.934.299.236.673.36 1.057.354h7.953a1.66 1.66 0 001.057-.354c.3-.235.504-.566.579-.934L19 9H5.09M9 3l3 3 3-3"
         stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
       />
     </svg>
   );
